5db2dabf9becba2dfa4c13f5490ee63cf716012c
Zimina Ivanna <zimina.ivanna@gmail.com>
2026-09-16 18:27:37 +0300

    feat(core): implement core business logic, game types, and rules

---
+++ RpsTournament/RpsTournament.Core/GameLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RpsTournament.Core
+{
+    public static class GameLogic
+    {
+        public static Move GetComputerMove()
+        {
+            // Arvuti teeb juhusliku valiku
+            Move[] moves = Enum.GetValues<Move>();
+            return moves[Random.Shared.Next(moves.Length)];
+        }
+
+        public static RoundResult GetResult(Move player, Move computer)
+        {
+            // Core sisaldab loogikat; siin määratakse vooru tulemus
+            if (player == computer) return RoundResult.Draw;
+            return (player, computer) switch
+            {
+                (Move.Rock, Move.Scissors) => RoundResult.Win,
+                (Move.Paper, Move.Rock) => RoundResult.Win,
+                (Move.Scissors, Move.Paper) => RoundResult.Win,
+                _ => RoundResult.Loss
+            };
+        }
+    }
+}
---
+++ RpsTournament/RpsTournament.Core/GameTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace RpsTournament.Core
+{
+    // Core sisaldab loogikat; enum-tüübid Move ja RoundResult ning struct-tüüp GameRound
+    public enum Move { Rock, Paper, Scissors }
+
+    public enum RoundResult { Win, Loss, Draw }
+
+    public struct GameRound
+    {
+        public int Number { get; set; }
+        public Move PlayerMove { get; set; }
+        public Move ComputerMove { get; set; }
+        public RoundResult Result { get; set; }
+    }
+}
